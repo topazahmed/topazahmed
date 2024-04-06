03883db3b4c0686f257190899699c796d228b99d
Topaz Ahmed <thisistopaz@hotmail.com>
2024-04-06 04:32:49 -0600

    react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo1.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+   My app coming soon...
+        </p>
+        <a
+          className="App-link"
+          href="https://topazahmed.wordpress.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go here for now
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
